31a3ed8c1665d168f4b88de4f624541a450ba199
CongSon2003 <sonchuan1@gmail.com>
2023-09-02 17:16:31 +0700

    H

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 let port = 3000;
 const path = require('path');
 
-app.use('/',express.static(path.join(__dirname,'/public')));
+app.use('/public',express.static(path.join(__dirname,'/public')));
 
 app.get('/',(req,res,next)=>{
     var linkfile = path.join(__dirname,'home.html');
